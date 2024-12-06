86030c77152275bf7603a627b8c1607132c8ec06
gizmohub-lab <gizmo.hub.in@gmail.com>
2024-12-06 11:28:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bookingRoutes = require('./routes/booking');
 const adminRoutes = require('./routes/admin');
+const path = require ('path');
 
 // Initialize express app
 const app = express();
