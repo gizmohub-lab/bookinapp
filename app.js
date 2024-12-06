1819cc61348e26f089be5fdaf4c81d3c24204933
gizmohub-lab <gizmo.hub.in@gmail.com>
2024-12-06 11:26:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// Set the path for views
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // MongoDB connection
